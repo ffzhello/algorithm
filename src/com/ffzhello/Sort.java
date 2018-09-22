0f3b1e3fc51f88412a8f77cf4982ba31e7a4b5d7
ffzhello <314094371@qq.com>
2018-09-22 22:31:07 +0800

    sort

---
+++ src/com/ffzhello/Sort.java
@@ -0,0 +1,8 @@
+package com.ffzhello;
+
+/**
+ * Created by ffzheng on 2018/9/22.
+ */
+public interface Sort<T> {
+    public void f();
+}
---
+++ src/com/ffzhello/sort/SimpleSelectSort.java
@@ -0,0 +1,7 @@
+package com.ffzhello.sort;
+
+/**
+ * Created by ffzheng on 2018/9/22.
+ */
+public class SimpleSelectSort {
+}
