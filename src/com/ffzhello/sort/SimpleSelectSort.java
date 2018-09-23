dbf06446e134d1dd786d268bb755803201d392c6
ffzhello <314094371@qq.com>
2018-09-23 19:39:16 +0800

    Sort接口方法定义

--- src/com/ffzhello/sort/SimpleSelectSort.java
+++ src/com/ffzhello/sort/SimpleSelectSort.java
@@ -1,7 +1,46 @@
 package com.ffzhello.sort;
 
 /**
+ * 简单选择排序
  * Created by ffzheng on 2018/9/22.
  */
-public class SimpleSelectSort {
+public class SimpleSelectSort implements Sort {
+    /**
+     * @param arr：待排序数组
+     * @param pattern:  desc(降序)、asc(升序)
+     * @return
+     */
+    @Override
+    public Object[] sort(Object[] arr, String pattern) {
+
+        // verify params
+        if (arr == null) {
+            return null;
+        }
+
+        if (arr.length == 0) {
+            return arr;
+        }
+
+        //desc
+        if (pattern != null && "desc".equals(pattern)) {
+            //this.sort(arr);
+            Object max ;
+            int lenth = arr.length;
+            for (int i = 0; i < lenth; i++) {
+                max = arr[i];
+                for (int j = i + 1; j < lenth; j ++) {
+
+                }
+
+            }
+
+        } else { //asc
+
+
+        }
+        return arr;
+    }
+
+
 }
--- src/com/ffzhello/sort/Sort.java
+++ src/com/ffzhello/sort/Sort.java
@@ -8,18 +8,86 @@ package com.ffzhello.sort;
 public interface Sort {
 
     /**
-     * 排序
+     * 字节数组排序
      * @param arr：待排序数组
-     * @param pattern:  desc(降序)、asc(升序)
+     * @param pattern:  false(降序)、true(升序)
      * @return 排序结果
      */
-    public Object[] sort(Object[] arr, String pattern);
+    public byte[] sort(byte[] arr, boolean pattern);
+
+    public default byte[] sort(byte[] arr) {
+        return sort(arr, true);
+    }
+
     /**
-     * 默认升序排序
+     * 字符数组排序
      * @param arr
+     * @param pattern
      * @return
      */
-    public default Object[] sort(Object[] arr) {
-        return sort(arr, "asc");
+    public char[] sort(char[] arr, boolean pattern);
+
+    public default char[] sort(char[] arr) {
+        return sort(arr, true);
+    }
+
+    /**
+     * short数组排序
+     * @param arr
+     * @param pattern
+     * @return
+     */
+    public short[] sort(short[] arr, boolean pattern);
+
+    public default short[] sort(short[] arr) {
+        return sort(arr, true);
+    }
+
+    /**
+     * int数组排序
+     * @param arr
+     * @param pattern
+     * @return
+     */
+    public int[] sort(int[] arr, boolean pattern);
+
+    public default int[] sort(int[] arr) {
+        return sort(arr, true);
+    }
+
+    /**
+     * long数组排序
+     * @param arr
+     * @param pattern
+     * @return
+     */
+    public long[] sort(long[] arr, boolean pattern);
+
+    public default long[] sort(long[] arr) {
+        return sort(arr,true);
+    }
+
+    /**
+     * float数组排序
+     * @param arr
+     * @param pattern
+     * @return
+     */
+    public float[] sort(float[] arr, boolean pattern);
+
+    public default float[] sort(float[] arr) {
+        return sort(arr, true);
+    }
+
+    /**
+     * double数组排序
+     * @param arr
+     * @param pattern
+     * @return
+     */
+    public double[] sort(double[] arr, boolean pattern);
+
+    public default double[] sort(double[] arr) {
+        return sort(arr,true);
     }
 }
