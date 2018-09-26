3671d75db2cdb7fd41fbbaf3b18b1dbe37064925
ffzhello <314094371@qq.com>
2018-09-26 10:18:33 +0800

    直接插入排序

--- src/com/ffzhello/sort/DirectInsertSort.java
+++ src/com/ffzhello/sort/DirectInsertSort.java
@@ -1,5 +1,7 @@
 package com.ffzhello.sort;
 
+import com.ffzhello.utils.ArrayUtils;
+
 /**
  * Created by ffzheng on 2018/9/26.
  */
@@ -13,59 +15,193 @@ public class DirectInsertSort implements Sort {
             return arr;
         }
 
-        for (int i = 1; i < len; i++) {
-            byte tmp = arr[i];
-            for (int k = i-1; k >= 0; k--) {
-                if (tmp > arr[k]) {
-                    for (int index = i; index > k; index--) {
-                        arr[index] = arr[index-1];
-                    }
-                    arr[k+1] = tmp;
+        int i, j, k;
+
+        for (i = 1; i < len; i++) {
+            for (j = i-1; j >= 0; j--) {
+                if (arr[j] < arr[i])
                     break;
-                }
+            }
+
+            if (j != i-1) {
+                byte tmp = arr[i];
+                for (k = i - 1; k > j; k--)
+                    arr[k+1] = arr[k];
+
+                arr[k+1] = tmp;
             }
         }
 
-        if (!pattern) {
-            int start = 0, end = len - 1;
-            while (start < end) {
-                byte tmp = arr[start];
-                arr[start] = arr[end];
-                arr[end] = tmp;
-            }
-        }
-
-        return arr;
+        //是否倒排
+        return pattern? arr: ArrayUtils.reverse(arr);
     }
 
     @Override
     public char[] sort(char[] arr, boolean pattern) {
-        return new char[0];
+        int len = arr.length;
+        if (len == 0 || len == 1) {
+            return arr;
+        }
+
+        int i, j, k;
+
+        for (i = 1; i < len; i++) {
+            for (j = i-1; j >= 0; j--) {
+                if (arr[j] < arr[i])
+                    break;
+            }
+
+            if (j != i-1) {
+                char tmp = arr[i];
+                for (k = i - 1; k > j; k--)
+                    arr[k+1] = arr[k];
+
+                arr[k+1] = tmp;
+            }
+        }
+
+        //是否倒排
+        return pattern? arr: ArrayUtils.reverse(arr);
     }
 
     @Override
     public short[] sort(short[] arr, boolean pattern) {
-        return new short[0];
+        int len = arr.length;
+        if (len == 0 || len == 1) {
+            return arr;
+        }
+
+        int i, j, k;
+
+        for (i = 1; i < len; i++) {
+            for (j = i-1; j >= 0; j--) {
+                if (arr[j] < arr[i])
+                    break;
+            }
+
+            if (j != i-1) {
+                short tmp = arr[i];
+                for (k = i - 1; k > j; k--)
+                    arr[k+1] = arr[k];
+
+                arr[k+1] = tmp;
+            }
+        }
+
+        //是否倒排
+        return pattern? arr: ArrayUtils.reverse(arr);
     }
 
     @Override
     public int[] sort(int[] arr, boolean pattern) {
-        return new int[0];
+        int len = arr.length;
+        if (len == 0 || len == 1) {
+            return arr;
+        }
+
+        int i, j, k;
+
+        for (i = 1; i < len; i++) {
+            for (j = i-1; j >= 0; j--) {
+                if (arr[j] < arr[i])
+                    break;
+            }
+
+            if (j != i-1) {
+                int tmp = arr[i];
+                for (k = i - 1; k > j; k--)
+                    arr[k+1] = arr[k];
+
+                arr[k+1] = tmp;
+            }
+        }
+
+        //是否倒排
+        return pattern? arr: ArrayUtils.reverse(arr);
     }
 
     @Override
     public long[] sort(long[] arr, boolean pattern) {
-        return new long[0];
+        int len = arr.length;
+        if (len == 0 || len == 1) {
+            return arr;
+        }
+
+        int i, j, k;
+
+        for (i = 1; i < len; i++) {
+            for (j = i-1; j >= 0; j--) {
+                if (arr[j] < arr[i])
+                    break;
+            }
+
+            if (j != i-1) {
+                long tmp = arr[i];
+                for (k = i - 1; k > j; k--)
+                    arr[k+1] = arr[k];
+
+                arr[k+1] = tmp;
+            }
+        }
+
+        //是否倒排
+        return pattern? arr: ArrayUtils.reverse(arr);
     }
 
     @Override
     public float[] sort(float[] arr, boolean pattern) {
-        return new float[0];
+        int len = arr.length;
+        if (len == 0 || len == 1) {
+            return arr;
+        }
+
+        int i, j, k;
+
+        for (i = 1; i < len; i++) {
+            for (j = i-1; j >= 0; j--) {
+                if (arr[j] < arr[i])
+                    break;
+            }
+
+            if (j != i-1) {
+                float tmp = arr[i];
+                for (k = i - 1; k > j; k--)
+                    arr[k+1] = arr[k];
+
+                arr[k+1] = tmp;
+            }
+        }
+
+        //是否倒排
+        return pattern? arr: ArrayUtils.reverse(arr);
     }
 
     @Override
     public double[] sort(double[] arr, boolean pattern) {
-        return new double[0];
+        int len = arr.length;
+        if (len == 0 || len == 1) {
+            return arr;
+        }
+
+        int i, j, k;
+
+        for (i = 1; i < len; i++) {
+            for (j = i-1; j >= 0; j--) {
+                if (arr[j] < arr[i])
+                    break;
+            }
+
+            if (j != i-1) {
+                double tmp = arr[i];
+                for (k = i - 1; k > j; k--)
+                    arr[k+1] = arr[k];
+
+                arr[k+1] = tmp;
+            }
+        }
+
+        //是否倒排
+        return pattern? arr: ArrayUtils.reverse(arr);
     }
 
     public static void main(String[] args) {
@@ -74,5 +210,10 @@ public class DirectInsertSort implements Sort {
         b = directInsertSort.sort(b);
         for (byte bb: b)
             System.out.println(bb);
+
+        double[] d = {3.1, 2.2, 4, 0.1};
+        d = directInsertSort.sort(d);
+        for (double dd: d)
+            System.out.println(dd);
     }
 }
